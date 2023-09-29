664077c538bfb0b56a21b8f8df15667cd1e98919
Rico Mariani <ricomariani@outlook.com>
2023-09-28 19:16:53 -0700

    Added comments to valid move generator

--- valid.c
+++ valid.c
@@ -1,9 +1,9 @@
 #include "board.h"
 
-#define stuff(a, b, c)                                                         \
-  (((((unsigned long)a) & 0xff) << 16) | ((((unsigned long)b) & 0xff) << 8) |  \
-   ((c)&0xff))
-#define unstuff(a) ((((a) >> 16) | ((a) >> 8) | (a)) & 0xff)
+#define tobyte(x) ((unsigned char)(x))
+
+#define load_state(a, b, c) ((tobyte(a) << 16) | (tobyte(b) << 8) | tobyte(c))
+#define unload_state(a) tobyte(((a) >> 16) | ((a) >> 8) | (a))
 
 int valid(BOARD board, int colour, int stack)
 {
@@ -48,34 +48,124 @@ int valid(BOARD board, int colour, int stack)
 
     unsigned int y0, y1, d0, d1;
 
-    y1 = stuff(used, used, used);
+    // We think of the matching as a regular expression
+    // and build the state machine to match it. If E is enemy, B
+    // is blank and M is mine then the match pattern is BH+M.
+    //
+    // Starting empty (state 0) then finding enemy piece (state 1)
+    // the a piece of mine (state 3 if found), once the match fails
+    // we go to state 2 and stay there. Actually we speed this up
+    // by starting in state 2 if the current spot is occupied and
+    // then only reading rows from there.  The machine is below:
+    //
+    // input alphabet:
+    //
+    // blank  = 0
+    // enemy  = 1
+    // me     = 2
+    // ignore = 3  this doesn't happen it's empty, black, or white, not both
+    //
+    // state transitions:
+    //
+    // X indicates don't care because it can't happen
+    // so we can pick whatever is convenient
+    //
+    // state is represented in two bits y0 (lo) and y1 (hi)
+    // alpha is represented in two bits d0 (lo) and d1 (hi)
+    //
+    //                  h m
+    //          input 0 1 2 3   00 01 10 x
+    // state 0:       2 1 2 X   10 01 10 x
+    // state 1:       3 1 2 X   11 01 10 x
+    // state 2:       2 2 2 X   10 10 10 x
+    // state 3:       3 3 3 X   11 11 11 x  VALID MOVE END STATE
+
+    // y0 transitions all bits in karnaugh map order
+    //                          BEST FREE CHOICE
+    //           00 01 11 10    00 01 11 10
+    // state 00:  0  1  x  0     0  1  1  0
+    // state 01:  1  1  x  0     1  1  1  0
+    // state 11:  1  1  x  1     1  1  1  1 VALID MOVE END STATE
+    // state 10:  0  0  x  0     0  0  0  0
+    //
+    // we see that we can cover the true cases with:
+    //  a row  y0 & y1
+    //  a box  y0 & d1
+    //       these two are compose to y0 & (y1|d1)
+    //  a box  ~y1 & d0
+    //
+    //  net expression:  y0 = (~y1 & d0) | (y0 & (y1|d1))
+    //
+    // y1 transitions all bits in karnaugh map order
+    //
+    //                         BEST FREE CHOICE
+    //           00 01 11 10    00 01 11 10
+    // state 00:  1  0  x  1     1  0  1  1
+    // state 01:  1  0  x  1     1  0  1  1
+    // state 11:  1  1  x  1     1  1  1  1  VALID MOVE END STATE
+    // state 10:  1  1  x  1     1  1  1  1
+    //
+    // we can cover those cases with
+    // bottom half : y1
+    // right  half : d1
+    // first and last column:  ~d0 (the old wrap around trick)
+    //
+    // net expression y1 = y1 | d1 | ~d0
+    // i.e.  y1 |= d1 | ~d0
+    //
+    // Those are exactly the binary operations below to run the state machine
+    //
+    // To find the valid bits, those are state3 or 11 so we just do y0 &= y1
+    // any bits that are set are valid moves.
+    //
+    // I'm writing this comment 36 years after I wrote this code
+    // and I'm stunned that it worked out on the first go...
+
+    y1 = load_state(used, used, used);
     y0 = 0;
     for (int i = y - 1; i >= 0; i--) {
-      d0 = stuff(him[i] >> (y - i), him[i] << (y - i), him[i]);
-      d1 = stuff(me[i] >> (y - i), me[i] << (y - i), me[i]);
+      // looking up, we compute straight up right diagonal, and left diagonal in parallel
+      d0 = load_state(him[i] >> (y - i), him[i] << (y - i), him[i]);
+      d1 = load_state(me[i] >> (y - i), me[i] << (y - i), me[i]);
+
+      // state machine logic see above
       y0 = ((~y1) & d0) | (y0 & (y1 | d1));
       y1 |= d1 | (~d0);
+
+      // when y1 is set the computation is finished either way, if they are all finished
+      // then we can bail out.
       if ((~y1) == 0)
         break;
     }
+    // read out: state 3 is valid move
     y0 &= y1;
-    row = unstuff(y0);
+    row = unload_state(y0);  // merge the successes from the 3 directions
 
-    y1 = stuff(used | row, used | row, used | row);
+    // now look the other direction but consider the moves found above to be filled
+    y1 = load_state(used | row, used | row, used | row);
     y0 = 0;
     for (int i = y + 1; i < 8; i++) {
-      d0 = stuff(him[i] >> (i - y), him[i] << (i - y), him[i]);
-      d1 = stuff(me[i] >> (i - y), me[i] << (i - y), me[i]);
+      // looking down, we compute straight up right diagonal, and left diagonal in parallel
+      d0 = load_state(him[i] >> (i - y), him[i] << (i - y), him[i]);
+      d1 = load_state(me[i] >> (i - y), me[i] << (i - y), me[i]);
+
+      // state machine logic see above
       y0 = ((~y1) & d0) | (y0 & (y1 | d1));
       y1 |= d1 | (~d0);
+
+      // when y1 is set the computation is finished either way, if they are all finished
+      // then we can bail out.
       if ((~y1) == 0)
         break;
     }
 
+    // read out: state 3 is valid move
     y0 &= y1;
-    row |= unstuff(y0);
+    row |= unload_state(y0);
     row &= ~(used);
     used |= row;
+
+    // now if anything was found, log it
     if (row) {
       for (int i = 0; i < 8; i++) {
         if (row & (1 << i)) {
