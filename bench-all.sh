b5b48310b25817afbd0b4025183ab15eec87d4fe
Rico Mariani <ricomariani@outlook.com>
2024-01-17 15:53:28 -0800

    rename wrath-sharp to wrath-sharp-vec128
    The first version I did was the vector128 version which turns out to be really bad
    but for historical reasons it had the best name and people often look at it first
    which is a horrible idea.
    
    I removed it from the standard benchmark script and renamed the directory to indicate
    that it is what it is.

--- bench-all.sh
+++ bench-all.sh
@@ -1,12 +1,17 @@
 echo "native benchmark (the control)"
 ./wrath xlt endgame.txt
-cd wrath-sharp
-echo "wrath sharp on .NET 6.0 using Vector128"
-dotnet run -c Release -f net6.0 xlt ../endgame.txt
-echo "wrath sharp on .NET 7.0 using Vector128"
-dotnet run -c Release -f net7.0 xlt ../endgame.txt
-echo "wrath sharp on .NET 8.0 using Vector128"
-dotnet run -c Release -f net8.0 xlt ../endgame.txt
+
+# This version is known to be quite poor and it really isn't very interesting
+# It was an interesting idea that turns out to be really kinda bad
+#
+# cd wrath-sharp-vec128
+# echo "wrath sharp on .NET 6.0 using Vector128"
+# dotnet run -c Release -f net6.0 xlt ../endgame.txt
+# echo "wrath sharp on .NET 7.0 using Vector128"
+# dotnet run -c Release -f net7.0 xlt ../endgame.txt
+# echo "wrath sharp on .NET 8.0 using Vector128"
+# dotnet run -c Release -f net8.0 xlt ../endgame.txt
+
 cd ../wrath-sharp-inline-arrays
 echo "wrath sharp on .NET 8.0 using inline arrays"
 dotnet run -c Release xlt ../endgame.txt
